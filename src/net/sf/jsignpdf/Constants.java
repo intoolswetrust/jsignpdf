9e71f59fde54019b07e79957169cee642c8fce54
kwart <kwart>
2008-03-25 14:39:20 +0000

    version 0.5

--- src/net/sf/jsignpdf/Constants.java
+++ src/net/sf/jsignpdf/Constants.java
@@ -9,7 +9,7 @@ public class Constants {
 	/**
 	 * Version of JSignPdf
 	 */
-	public static final String VERSION = "0.3";
+	public static final String VERSION = "0.5";
 
 	/**
 	 * Home directory of current user. It's not a real constant
