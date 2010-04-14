73fbfb7c67a9524bedd7d92628d314f332683f6d
kwart <kwart>
2010-04-14 15:40:07 +0000

    fix for embedded font problem in PDF-1.2 (use the version 1.3)

--- src/net/sf/jsignpdf/SignerLogic.java
+++ src/net/sf/jsignpdf/SignerLogic.java
@@ -31,6 +31,7 @@ import com.lowagie.text.pdf.TSAClientBouncyCastle;
 
 /**
  * Main logic of signer application. It uses iText to create signature in PDF.
+ * 
  * @author Josef Cacek
  */
 public class SignerLogic implements Runnable {
@@ -41,21 +42,25 @@ public class SignerLogic implements Runnable {
 
 	/**
 	 * Constructor with all necessary parameters.
-	 * @param anOptions options of signer
+	 * 
+	 * @param anOptions
+	 *            options of signer
 	 */
 	public SignerLogic(final BasicSignerOptions anOptions) {
 		options = anOptions;
 	}
 
-	/* (non-Javadoc)
+	/*
+	 * (non-Javadoc)
+	 * 
 	 * @see java.lang.Runnable#run()
 	 */
 	@SuppressWarnings("unchecked")
 	public void run() {
-		if (options==null) {
+		if (options == null) {
 			throw new NullPointerException("Options has to be filled.");
 		}
-		if (! validateInOutFiles(options.getInFile(), options.getOutFile())) {
+		if (!validateInOutFiles(options.getInFile(), options.getOutFile())) {
 			options.log("console.skippingSigning");
 			return;
 		}
@@ -68,15 +73,13 @@ public class SignerLogic implements Runnable {
 			options.log("console.createPdfReader", options.getInFile());
 			PdfReader reader;
 			try {
-				//try to read without password
+				// try to read without password
 				reader = new PdfReader(options.getInFile());
 			} catch (Exception e) {
 				try {
-					reader = new PdfReader(options.getInFile(),
-							new byte[0]);
+					reader = new PdfReader(options.getInFile(), new byte[0]);
 				} catch (Exception e2) {
-					reader = new PdfReader(options.getInFile(),
-							options.getPdfOwnerPwdStr().getBytes());
+					reader = new PdfReader(options.getInFile(), options.getPdfOwnerPwdStr().getBytes());
 				}
 			}
 
@@ -84,24 +87,27 @@ public class SignerLogic implements Runnable {
 			final FileOutputStream fout = new FileOutputStream(options.getOutFile());
 
 			options.log("console.createSignature");
-			final PdfStamper stp =
-				PdfStamper.createSignature(reader, fout, '\0', null, options.isAppendX());
-
+			char tmpPdfVersion = '\0'; // default version - the same as input
+			if (options.isVisible() && !options.isAppendX()) {
+				// fix the problem with embedded font name in older PDF versions
+				char tmpOldVersion = reader.getPdfVersion();
+				if (tmpOldVersion < PdfWriter.VERSION_1_3) {
+					tmpPdfVersion = PdfWriter.VERSION_1_3;
+					options.log("console.updateVersion", new String[] { String.valueOf(tmpOldVersion),
+							String.valueOf(tmpPdfVersion) });
+				}
+			}
+			final PdfStamper stp = PdfStamper.createSignature(reader, fout, tmpPdfVersion, null, options.isAppendX());
 			if (options.isEncryptedX()) {
 				options.log("console.setEncryption");
 				final int tmpRight = options.getRightPrinting().getRight()
-					| (options.isRightCopy() ? PdfWriter.ALLOW_COPY: 0)
-					| (options.isRightAssembly() ? PdfWriter.ALLOW_ASSEMBLY: 0)
-					| (options.isRightFillIn() ? PdfWriter.ALLOW_FILL_IN: 0)
-					| (options.isRightScreanReaders() ? PdfWriter.ALLOW_SCREENREADERS: 0)
-					| (options.isRightModifyAnnotations() ? PdfWriter.ALLOW_MODIFY_ANNOTATIONS: 0)
-					| (options.isRightModifyContents() ? PdfWriter.ALLOW_MODIFY_CONTENTS: 0)
-					;
-				stp.setEncryption(true,
-					options.getPdfUserPwdStr(),
-					options.getPdfOwnerPwdStr(),
-					tmpRight
-					);
+						| (options.isRightCopy() ? PdfWriter.ALLOW_COPY : 0)
+						| (options.isRightAssembly() ? PdfWriter.ALLOW_ASSEMBLY : 0)
+						| (options.isRightFillIn() ? PdfWriter.ALLOW_FILL_IN : 0)
+						| (options.isRightScreanReaders() ? PdfWriter.ALLOW_SCREENREADERS : 0)
+						| (options.isRightModifyAnnotations() ? PdfWriter.ALLOW_MODIFY_ANNOTATIONS : 0)
+						| (options.isRightModifyContents() ? PdfWriter.ALLOW_MODIFY_CONTENTS : 0);
+				stp.setEncryption(true, options.getPdfUserPwdStr(), options.getPdfOwnerPwdStr(), tmpRight);
 			}
 
 			final PdfSignatureAppearance sap = stp.getSignatureAppearance();
@@ -122,7 +128,7 @@ public class SignerLogic implements Runnable {
 			sap.setCertificationLevel(options.getCertLevelX().getLevel());
 
 			if (options.isVisible()) {
-				//visible signature is enabled
+				// visible signature is enabled
 				options.log("console.configureVisible");
 				options.log("console.setAcro6Layers");
 				sap.setAcro6Layers(true);
@@ -145,27 +151,20 @@ public class SignerLogic implements Runnable {
 				sap.setImageScale(options.getBgImgScale());
 				options.log("console.setL2Text");
 				sap.setLayer2Text(options.getL2Text());
-				if (getL2BaseFont()!=null) {
-					sap.setLayer2Font(new Font(getL2BaseFont(),options.getL2TextFontSize()));
+				if (getL2BaseFont() != null) {
+					sap.setLayer2Font(new Font(getL2BaseFont(), options.getL2TextFontSize()));
 				}
 				options.log("console.setL4Text");
 				sap.setLayer4Text(options.getL4Text());
 				options.log("console.setRender");
 				sap.setRender(options.getRenderMode().getRender());
 				options.log("console.setVisibleSignature");
-				sap.setVisibleSignature(
-					new Rectangle(
-						options.getPositionLLX(),
-						options.getPositionLLY(),
-						options.getPositionURX(),
-						options.getPositionURY()),
-					options.getPage(),
-					null);
+				sap.setVisibleSignature(new Rectangle(options.getPositionLLX(), options.getPositionLLY(), options
+						.getPositionURX(), options.getPositionURY()), options.getPage(), null);
 			}
 
 			options.log("console.processing");
-			final PdfSignature dic = new PdfSignature(PdfName.ADOBE_PPKLITE,
-					new PdfName("adbe.pkcs7.detached"));
+			final PdfSignature dic = new PdfSignature(PdfName.ADOBE_PPKLITE, new PdfName("adbe.pkcs7.detached"));
 			dic.setReason(sap.getReason());
 			dic.setLocation(sap.getLocation());
 			dic.setContact(sap.getContact());
@@ -193,19 +192,17 @@ public class SignerLogic implements Runnable {
 				String url = PdfPKCS7.getOCSPURL((X509Certificate) chain[0]);
 				if (url != null && url.length() > 0) {
 					options.log("console.readingOCSP");
-					ocsp = new OcspClientBouncyCastle(
-							(X509Certificate) chain[0],
-							(X509Certificate) chain[1], url).getEncoded();
+					ocsp = new OcspClientBouncyCastle((X509Certificate) chain[0], (X509Certificate) chain[1], url)
+							.getEncoded();
 				}
 			}
 			byte sh[] = sgn.getAuthenticatedAttributeBytes(hash, cal, ocsp);
 			sgn.update(sh, 0, sh.length);
 
 			TSAClient tsc = null;
-			if (options.isTimestampX() && ! StringUtils.isEmpty(options.getTsaUrl())) {
+			if (options.isTimestampX() && !StringUtils.isEmpty(options.getTsaUrl())) {
 				options.log("console.creatingTsaClient");
-				tsc = new TSAClientBouncyCastle(options.getTsaUrl(),
-						StringUtils.emptyNull(options.getTsaUser()),
+				tsc = new TSAClientBouncyCastle(options.getTsaUrl(), StringUtils.emptyNull(options.getTsaUser()),
 						StringUtils.emptyNull(options.getTsaPasswd()));
 			}
 			byte[] encodedSig = sgn.getEncodedPKCS7(hash, cal, tsc, ocsp);
@@ -217,8 +214,7 @@ public class SignerLogic implements Runnable {
 			System.arraycopy(encodedSig, 0, paddedSig, 0, encodedSig.length);
 
 			PdfDictionary dic2 = new PdfDictionary();
-			dic2.put(PdfName.CONTENTS, new PdfString(paddedSig)
-					.setHexWriting(true));
+			dic2.put(PdfName.CONTENTS, new PdfString(paddedSig).setHexWriting(true));
 			options.log("console.closeStream");
 			sap.close(dic2);
 			fout.close();
@@ -231,28 +227,30 @@ public class SignerLogic implements Runnable {
 			e.printStackTrace(options.getPrintWriter());
 			options.log("console.memoryError");
 		}
-		options.log("console.finished." + (tmpResult?"ok":"error"));
+		options.log("console.finished." + (tmpResult ? "ok" : "error"));
 		options.fireSignerFinishedEvent(tmpResult);
 	}
 
-
 	/**
 	 * Validates if input and output files are valid for signing.
-	 * @param inFile input file
-	 * @param outFile output file
+	 * 
+	 * @param inFile
+	 *            input file
+	 * @param outFile
+	 *            output file
 	 * @return true if valid, false otherwise
 	 */
 	private boolean validateInOutFiles(final String inFile, final String outFile) {
-		options.log("console.validatingFiles");		
+		options.log("console.validatingFiles");
 		if (StringUtils.isEmpty(inFile) || StringUtils.isEmpty(outFile)) {
 			options.log("console.fileNotFilled.error");
 			return false;
 		}
 		final File tmpInFile = new File(inFile);
 		final File tmpOutFile = new File(outFile);
-		if (! (tmpInFile.exists() && tmpInFile.isFile() && tmpInFile.canRead()) ) {
+		if (!(tmpInFile.exists() && tmpInFile.isFile() && tmpInFile.canRead())) {
 			options.log("console.inFileNotFound.error");
-			return false;			
+			return false;
 		}
 		if (tmpInFile.getAbsolutePath().equals(tmpOutFile.getAbsolutePath())) {
 			options.log("console.filesAreEqual.error");
@@ -263,24 +261,24 @@ public class SignerLogic implements Runnable {
 
 	/**
 	 * Returns BaseFont for text of visible signature;
+	 * 
 	 * @return
 	 */
 	public static synchronized BaseFont getL2BaseFont() {
-		if (l2baseFont==null) {
+		if (l2baseFont == null) {
 			try {
 				final ByteArrayOutputStream tmpBaos = new ByteArrayOutputStream();
 				InputStream tmpIs = SignerLogic.class.getResourceAsStream(Constants.L2TEXT_FONT_PATH);
 				IOUtils.copy(tmpIs, tmpBaos);
 				tmpIs.close();
 				tmpBaos.close();
-				l2baseFont = BaseFont.createFont(Constants.L2TEXT_FONT_NAME, BaseFont.IDENTITY_H,
-						BaseFont.EMBEDDED, BaseFont.CACHED,
-						tmpBaos.toByteArray(), null);
+				l2baseFont = BaseFont.createFont(Constants.L2TEXT_FONT_NAME, BaseFont.IDENTITY_H, BaseFont.EMBEDDED,
+						BaseFont.CACHED, tmpBaos.toByteArray(), null);
 			} catch (Exception e) {
 				try {
 					l2baseFont = BaseFont.createFont(BaseFont.HELVETICA, BaseFont.CP1250, BaseFont.EMBEDDED);
 				} catch (Exception ex) {
-					//where is the problem, dear Watson?
+					// where is the problem, dear Watson?
 				}
 			}
 		}
