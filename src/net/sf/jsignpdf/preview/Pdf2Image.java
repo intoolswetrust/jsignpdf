c7a05e30e8d3e1bbe1557e7fdd15a607e2edcd2b
kwart <kwart>
2010-06-11 08:12:39 +0000

    Use PDFbox image rendering if Sun PdfRenderer fails

--- src/net/sf/jsignpdf/preview/Pdf2Image.java
+++ src/net/sf/jsignpdf/preview/Pdf2Image.java
@@ -1,6 +1,8 @@
 package net.sf.jsignpdf.preview;
 
+import java.awt.HeadlessException;
 import java.awt.Rectangle;
+import java.awt.Toolkit;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.FileNotFoundException;
@@ -11,8 +13,13 @@ import java.nio.channels.FileChannel;
 
 import net.sf.jsignpdf.BasicSignerOptions;
 
+import org.apache.pdfbox.pdmodel.PDDocument;
+import org.apache.pdfbox.pdmodel.PDPage;
+
 import com.sun.pdfview.PDFFile;
 import com.sun.pdfview.PDFPage;
+import com.sun.pdfview.PDFParseException;
+import com.sun.pdfview.decrypt.PDFPassword;
 
 /**
  * Helper class for converting a page in PDF to a {@link BufferedImage} object.
@@ -43,6 +50,22 @@ public class Pdf2Image {
 	 * @return image or null if error occures.
 	 */
 	public BufferedImage getImageForPage(final int aPage) {
+		BufferedImage tmpResult = getImageUsingPdfRenderer(aPage);
+		if (tmpResult == null) {
+			tmpResult = getImageUsingPdfBox(aPage);
+		}
+		return tmpResult;
+	}
+
+	/**
+	 * Returns image (or null if failed) generated from given page in PDF using
+	 * PDFBox tool.
+	 * 
+	 * @param aPage
+	 *            page in PDF (1 based)
+	 * @return image or null
+	 */
+	public BufferedImage getImageUsingPdfRenderer(final int aPage) {
 		BufferedImage tmpResult = null;
 		try {
 			// load a pdf from a byte buffer
@@ -50,7 +73,19 @@ public class Pdf2Image {
 			RandomAccessFile raf = new RandomAccessFile(file, "r");
 			FileChannel channel = raf.getChannel();
 			ByteBuffer buf = channel.map(FileChannel.MapMode.READ_ONLY, 0, channel.size());
-			PDFFile pdffile = new PDFFile(buf);
+			PDFFile pdffile = null;
+			try {
+				// try to read PDF without password
+				pdffile = new PDFFile(buf);
+			} catch (PDFParseException ppe) {
+				try {
+					// try to read PDF with empty password
+					pdffile = new PDFFile(buf, new PDFPassword(""));
+				} catch (PDFParseException ppe2) {
+					// try to read PDF with owner password
+					pdffile = new PDFFile(buf, new PDFPassword(options.getPdfOwnerPwdStr()));
+				}
+			}
 
 			// draw the first page to an image
 			PDFPage page = pdffile.getPage(aPage - 1);
@@ -71,6 +106,46 @@ public class Pdf2Image {
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
+		}
+		return tmpResult;
+	}
+
+	/**
+	 * Returns image (or null if failed) generated from given page in PDF using
+	 * PDFBox tool.
+	 * 
+	 * @param aPage
+	 *            page in PDF (1 based)
+	 * @return image or null
+	 */
+	public BufferedImage getImageUsingPdfBox(final int aPage) {
+		BufferedImage tmpResult = null;
+		PDDocument tmpDoc = null;
+
+		try {
+			tmpDoc = PDDocument.load(options.getInFile());
+			if (tmpDoc.isEncrypted()) {
+				tmpDoc.decrypt(options.getPdfOwnerPwdStr());
+			}
+			int resolution;
+			try {
+				resolution = Toolkit.getDefaultToolkit().getScreenResolution();
+			} catch (HeadlessException e) {
+				resolution = 96;
+			}
+
+			final PDPage page = (PDPage) tmpDoc.getDocumentCatalog().getAllPages().get(aPage - 1);
+			tmpResult = page.convertToImage(BufferedImage.TYPE_INT_RGB, resolution);
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			if (tmpDoc != null) {
+				try {
+					tmpDoc.close();
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
 		}
 		return tmpResult;
 	}
