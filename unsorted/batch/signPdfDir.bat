a030376c8bbc6a630b1b2def6d822842557f1445
kwart <kwart>
2010-07-13 18:29:32 +0000

    usage fix

--- unsorted/batch/signPdfDir.bat
+++ unsorted/batch/signPdfDir.bat
@@ -7,11 +7,11 @@ cmd "/V:ON" /C JSingPdf-signDir.cmd %1 %2 %3 %4 %5 %6 %7 %8 %9
 GOTO End
 
 :PrintHelp
-echo "Command line parameter is missing"
-echo ""
-echo "Usage:"
-echo "signPdfDir.bat <C:\Path\To\Pdf\Folder> <argumentForJSignPdf-1> <argumentForJSignPdf-2> ..."
-
+echo Command line parameter is missing
+echo .
+echo Usage:
+echo    signPdfDir.bat [C:\Path\To\Pdf\Folder] [argumentForJSignPdf-1] [argumentForJSignPdf-2] ...
+echo .
 
 :End
 
