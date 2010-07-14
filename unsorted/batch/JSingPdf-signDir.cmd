08c119c5b3e15d1af86d5d31fd362cfa0c756d0a
kwart <kwart>
2010-07-14 14:26:15 +0000

    Windows console launcher

--- unsorted/batch/JSingPdf-signDir.cmd
+++ unsorted/batch/JSingPdf-signDir.cmd
@@ -4,5 +4,5 @@ REM use with cmd "/V:ON"
 set FILES=
 for %%i in (%1\*.pdf) do set FILES=!FILES! "%%i"
 SHIFT
-echo jre\bin\java.exe -Duser.language=en -jar JSignPdf.jar %FILES% %1 %2 %3 %4 %5 %6 %7 %8
-jre\bin\java.exe -Duser.language=en -jar JSignPdf.jar %FILES% %1 %2 %3 %4 %5 %6 %7 %8
+echo JSignPdfC.exe %FILES% %1 %2 %3 %4 %5 %6 %7 %8
+JSignPdfC.exe %FILES% %1 %2 %3 %4 %5 %6 %7 %8
