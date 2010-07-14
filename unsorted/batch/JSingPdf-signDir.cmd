0f35b01aadaf939e46f97341b80c35d7c636e1d9
kwart <kwart>
2010-07-14 06:32:38 +0000

    use embedded jre

--- unsorted/batch/JSingPdf-signDir.cmd
+++ unsorted/batch/JSingPdf-signDir.cmd
@@ -4,5 +4,5 @@ REM use with cmd "/V:ON"
 set FILES=
 for %%i in (%1\*.pdf) do set FILES=!FILES! "%%i"
 SHIFT
-echo java -Duser.language=en -jar JSignPdf.jar %FILES% %1 %2 %3 %4 %5 %6 %7 %8
-java -Duser.language=en -jar JSignPdf.jar %FILES% %1 %2 %3 %4 %5 %6 %7 %8
+echo jre\bin\java.exe -Duser.language=en -jar JSignPdf.jar %FILES% %1 %2 %3 %4 %5 %6 %7 %8
+jre\bin\java.exe -Duser.language=en -jar JSignPdf.jar %FILES% %1 %2 %3 %4 %5 %6 %7 %8
