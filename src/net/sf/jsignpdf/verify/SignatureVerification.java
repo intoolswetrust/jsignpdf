f201c60df7d1f78be934ee0d4093f8b5b86e7082
stojsavljevic <stojsavljevic>
2011-04-01 10:57:51 +0000

    Removed certificateValid flag and introduced signCertTrustedAndValid flag together with validation status codes.

--- src/net/sf/jsignpdf/verify/SignatureVerification.java
+++ src/net/sf/jsignpdf/verify/SignatureVerification.java
@@ -9,221 +9,270 @@ import net.sf.jsignpdf.types.CertificationLevel;
  * This class represents a result of a single signature verification.
  * 
  * @author Josef Cacek
- * @author $Author: kwart $
- * @version $Revision: 1.6 $
- * @created $Date: 2011/03/31 10:45:48 $
+ * @author $Author: stojsavljevic $
+ * @version $Revision: 1.7 $
+ * @created $Date: 2011/04/01 10:57:51 $
  */
 public class SignatureVerification {
 
-	private String signName;
-	private String name;
-	private String subject;
-	private int revision;
-	private boolean wholeDocument;
-	private Calendar date;
-	private boolean modified;
-	private boolean ocspPresent;
-	private boolean ocspValid;
-	private boolean tsTokenPresent;
-	private Exception tsTokenValidationResult;
-	private Object[] fails;
-	private String reason;
-	private String location;
-	private int certLevelCode;
-	private boolean certificateValid;
+    /**
+     * Signed revision has been altered.
+     */
+    public static final Integer SIG_STAT_CODE_ERROR_REVISION_MODIFIED = new Integer(10);
 
-	/**
-	 * Default constructore
-	 */
-	public SignatureVerification() {
-		//nothing to do here
-	}
+    /**
+     * Revision has set certification level but document was modified later.
+     */
+    public static final Integer SIG_STAT_CODE_ERROR_CERTIFICATION_BROKEN = new Integer(11);
 
-	/**
-	 * Constructor, which fills name of signature
-	 * 
-	 * @param aName
-	 *            name of signature
-	 */
-	public SignatureVerification(final String aName) {
-		name = aName;
-	}
+    /**
+     * There is some unsigned content in document (last signature doesn't cover
+     * whole document).
+     */
+    public static final Integer SIG_STAT_CODE_WARNING_UNSIGNED_CONTENT = new Integer(20);
 
-	public String getName() {
-		return name;
-	}
+    /**
+     * Signature validity can't be verified.
+     */
+    public static final Integer SIG_STAT_CODE_WARNING_SIGNATURE_VALIDITY_UNKNOWN = new Integer(21);
 
-	public void setName(String name) {
-		this.name = name;
-	}
+    /**
+     * Signature is invalid according to OCSP.
+     */
+    public static final Integer SIG_STAT_CODE_WARNING_SIGNATURE_OCSP_INVALID = new Integer(22);
 
-	public String getSubject() {
-		return subject;
-	}
+    /**
+     * There is no timestamp token (signature date/time are from the clock on
+     * the signer's computer)
+     */
+    public static final Integer SIG_STAT_CODE_WARNING_NO_TIMESTAMP_TOKEN = new Integer(23);
 
-	public void setSubject(String subject) {
-		this.subject = subject;
-	}
+    /**
+     * Timestamp token is invalid.
+     */
+    public static final Integer SIG_STAT_CODE_WARNING_TIMESTAMP_INVALID = new Integer(24);
 
-	public int getRevision() {
-		return revision;
-	}
+    /**
+     * Signature is valid.
+     */
+    public static final Integer SIG_STAT_CODE_INFO_SIGNATURE_VALID = new Integer(30);
 
-	public void setRevision(int revision) {
-		this.revision = revision;
-	}
+    private String signName;
+    private String name;
+    private String subject;
+    private int revision;
+    private boolean wholeDocument;
+    private Calendar date;
+    private boolean modified;
+    private boolean ocspPresent;
+    private boolean ocspValid;
+    private boolean tsTokenPresent;
+    private Exception tsTokenValidationResult;
+    private Object[] fails;
+    private String reason;
+    private String location;
+    private int certLevelCode;
 
-	public boolean isWholeDocument() {
-		return wholeDocument;
-	}
+    /**
+     * This flag means that signing certificate is directly trusted (regarding
+     * to chosen KeyStore) and valid (it's not expired and doesn't appear in
+     * CRL).
+     */
+    private boolean signCertTrustedAndValid;
 
-	public void setWholeDocument(boolean wholeDocument) {
-		this.wholeDocument = wholeDocument;
-	}
+    /**
+     * Default constructore
+     */
+    public SignatureVerification() {
+        //nothing to do here
+    }
 
-	public Calendar getDate() {
-		return date;
-	}
+    /**
+     * Constructor, which fills name of signature
+     * 
+     * @param aName
+     *            name of signature
+     */
+    public SignatureVerification(final String aName) {
+        name = aName;
+    }
 
-	public void setDate(Calendar date) {
-		this.date = date;
-	}
+    public String getName() {
+        return name;
+    }
 
-	public boolean isModified() {
-		return modified;
-	}
+    public void setName(String name) {
+        this.name = name;
+    }
 
-	public void setModified(boolean modified) {
-		this.modified = modified;
-	}
+    public String getSubject() {
+        return subject;
+    }
 
-	public Object[] getFails() {
-		return fails;
-	}
+    public void setSubject(String subject) {
+        this.subject = subject;
+    }
 
-	public void setFails(Object[] fails) {
-		this.fails = fails;
-	}
+    public int getRevision() {
+        return revision;
+    }
 
-	public String getLocation() {
-		return location;
-	}
+    public void setRevision(int revision) {
+        this.revision = revision;
+    }
 
-	public void setLocation(String location) {
-		this.location = location;
-	}
+    public boolean isWholeDocument() {
+        return wholeDocument;
+    }
 
-	public String getReason() {
-		return reason;
-	}
+    public void setWholeDocument(boolean wholeDocument) {
+        this.wholeDocument = wholeDocument;
+    }
 
-	public void setReason(String reason) {
-		this.reason = reason;
-	}
+    public Calendar getDate() {
+        return date;
+    }
 
-	public boolean isOcspPresent() {
-		return ocspPresent;
-	}
+    public void setDate(Calendar date) {
+        this.date = date;
+    }
 
-	public void setOcspPresent(boolean ocspPresent) {
-		this.ocspPresent = ocspPresent;
-	}
+    public boolean isModified() {
+        return modified;
+    }
 
-	public boolean isOcspValid() {
-		return ocspValid;
-	}
+    public void setModified(boolean modified) {
+        this.modified = modified;
+    }
 
-	public void setOcspValid(boolean ocspValid) {
-		this.ocspValid = ocspValid;
-	}
+    public Object[] getFails() {
+        return fails;
+    }
 
-	public String getSignName() {
-		return signName;
-	}
+    public void setFails(Object[] fails) {
+        this.fails = fails;
+    }
 
-	public void setSignName(String signName) {
-		this.signName = signName;
-	}
+    public String getLocation() {
+        return location;
+    }
 
-	/**
-	 * @return the tsTokenPresent
-	 */
-	public boolean isTsTokenPresent() {
-		return tsTokenPresent;
-	}
+    public void setLocation(String location) {
+        this.location = location;
+    }
 
-	/**
-	 * @param tsTokenPresent
-	 *            the tsTokenPresent to set
-	 */
-	public void setTsTokenPresent(boolean tsTokenPresent) {
-		this.tsTokenPresent = tsTokenPresent;
-	}
+    public String getReason() {
+        return reason;
+    }
 
-	/**
-	 * @return the tsTokenValidationResult
-	 */
-	public Exception getTsTokenValidationResult() {
-		return tsTokenValidationResult;
-	}
+    public void setReason(String reason) {
+        this.reason = reason;
+    }
 
-	/**
-	 * @param tsTokenValidationResult
-	 *            the tsTokenValidationResult to set
-	 */
-	public void setTsTokenValidationResult(Exception tsTokenValidationResult) {
-		this.tsTokenValidationResult = tsTokenValidationResult;
-	}
+    public boolean isOcspPresent() {
+        return ocspPresent;
+    }
 
-	/**
-	 * @return the certLevelCode
-	 */
-	public int getCertLevelCode() {
-		return certLevelCode;
-	}
+    public void setOcspPresent(boolean ocspPresent) {
+        this.ocspPresent = ocspPresent;
+    }
 
-	/**
-	 * @param certLevelCode
-	 *            the certLevelCode to set
-	 */
-	public void setCertLevelCode(int certLevelCode) {
-		this.certLevelCode = certLevelCode;
-	}
+    public boolean isOcspValid() {
+        return ocspValid;
+    }
 
-	/**
-	 * Returns instance of {@link CertificationLevel} for the property
-	 * certLevelCode.
-	 * 
-	 * @return
-	 */
-	public CertificationLevel getCertificationLevel() {
-		return CertificationLevel.findCertificationLevel(certLevelCode);
-	}
+    public void setOcspValid(boolean ocspValid) {
+        this.ocspValid = ocspValid;
+    }
 
-	/**
-	 * @return the certificateValid
-	 */
-	public boolean isCertificateValid() {
-		return certificateValid;
-	}
+    public String getSignName() {
+        return signName;
+    }
 
-	/**
-	 * @param certificateValid
-	 *            the certificateValid to set
-	 */
-	public void setCertificateValid(boolean certificateValid) {
-		this.certificateValid = certificateValid;
-	}
+    public void setSignName(String signName) {
+        this.signName = signName;
+    }
 
-	public String toString() {
-		return "Signature verification [" + "\n signName=" + signName + "\n name=" + name + "\n subject=" + subject
-				+ "\n date=" + date.getTime() + "\n reason=" + reason + "\n location=" + location + "\n revision="
-				+ revision + "\n wholeDocument=" + wholeDocument + "\n modified=" + modified + "\n certificationLevel="
-				+ getCertificationLevel().name() + "\n certificateValid=" + certificateValid + "\n ocspPresent="
-				+ ocspPresent + "\n ocspValid=" + ocspValid + "\n timeStampTokenPresent=" + tsTokenPresent
-				+ "\n timeStampTokenValidationFail="
-				+ (tsTokenValidationResult == null ? "no" : tsTokenValidationResult.getMessage()) + "\n fails="
-				+ (fails == null ? "no" : Arrays.asList(fails)) + "\n]";
-	}
+    /**
+     * @return the tsTokenPresent
+     */
+    public boolean isTsTokenPresent() {
+        return tsTokenPresent;
+    }
+
+    /**
+     * @param tsTokenPresent
+     *            the tsTokenPresent to set
+     */
+    public void setTsTokenPresent(boolean tsTokenPresent) {
+        this.tsTokenPresent = tsTokenPresent;
+    }
+
+    /**
+     * @return the tsTokenValidationResult
+     */
+    public Exception getTsTokenValidationResult() {
+        return tsTokenValidationResult;
+    }
+
+    /**
+     * @param tsTokenValidationResult
+     *            the tsTokenValidationResult to set
+     */
+    public void setTsTokenValidationResult(Exception tsTokenValidationResult) {
+        this.tsTokenValidationResult = tsTokenValidationResult;
+    }
+
+    /**
+     * @return the certLevelCode
+     */
+    public int getCertLevelCode() {
+        return certLevelCode;
+    }
+
+    /**
+     * @param certLevelCode
+     *            the certLevelCode to set
+     */
+    public void setCertLevelCode(int certLevelCode) {
+        this.certLevelCode = certLevelCode;
+    }
+
+    /**
+     * Returns instance of {@link CertificationLevel} for the property
+     * certLevelCode.
+     * 
+     * @return
+     */
+    public CertificationLevel getCertificationLevel() {
+        return CertificationLevel.findCertificationLevel(certLevelCode);
+    }
+
+    /**
+     * 
+     * @return the signCertTrustedAndValid
+     */
+    public boolean isSignCertTrustedAndValid() {
+        return signCertTrustedAndValid;
+    }
+
+    /**
+     * @param signCertTrustedAndValid
+     *            the signCertTrustedAndValid to set
+     */
+    public void setSignCertTrustedAndValid(boolean signCertTrustedAndValid) {
+        this.signCertTrustedAndValid = signCertTrustedAndValid;
+    }
+
+    public String toString() {
+        return "Signature verification [" + "\n signName=" + signName + "\n name=" + name + "\n subject=" + subject
+                + "\n date=" + date.getTime() + "\n reason=" + reason + "\n location=" + location + "\n revision="
+                + revision + "\n wholeDocument=" + wholeDocument + "\n modified=" + modified + "\n certificationLevel="
+                + getCertificationLevel().name() + "\n signCertTrustedAndValid=" + signCertTrustedAndValid
+                + "\n ocspPresent=" + ocspPresent + "\n ocspValid=" + ocspValid + "\n timeStampTokenPresent="
+                + tsTokenPresent + "\n timeStampTokenValidationFail="
+                + (tsTokenValidationResult == null ? "no" : tsTokenValidationResult.getMessage()) + "\n fails="
+                + (fails == null ? "no" : Arrays.asList(fails)) + "\n]";
+    }
 
 }
