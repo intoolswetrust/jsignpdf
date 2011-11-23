499b80a8f17bf72a8fa38c270807234e2103accb
kwart <kwart>
2011-11-23 22:39:35 +0000

    print stacktrace if PKCS11 provider initialization fails

--- src/net/sf/jsignpdf/utils/PKCS11Utils.java
+++ src/net/sf/jsignpdf/utils/PKCS11Utils.java
@@ -11,34 +11,32 @@ import java.security.Security;
  */
 public class PKCS11Utils {
 
-	/**
-	 * Tries to register the sun.security.pkcs11.SunPKCS11 provider with
-	 * configuration provided in the given file.
-	 * 
-	 * @param configPath
-	 *            path to PKCS#11 provider configuration file
-	 * @return true if provider succesfully registered; false otherwise
-	 */
-	public static boolean registerProvider(final String configPath) {
-		if (StringUtils.isEmpty(configPath)) {
-			return false;
-		}
-		final File cfgFile = new File(configPath);
-		final String absolutePath = cfgFile.getAbsolutePath();
-		if (cfgFile.isFile()) {
-			try {
-				Security.addProvider((Provider) Class.forName("sun.security.pkcs11.SunPKCS11").getConstructor(
-						String.class).newInstance(absolutePath));
-				return true;
-			} catch (Exception e) {
-				System.err
-						.println("Unable to register SunPKCS11 security provider. The original exception message is:\n"
-								+ e.getMessage());
-			}
-		} else {
-			System.err.println("The PKCS#11 provider is not registered. Configuration file doesn't exist: "
-					+ absolutePath);
-		}
-		return false;
-	}
+  /**
+   * Tries to register the sun.security.pkcs11.SunPKCS11 provider with
+   * configuration provided in the given file.
+   * 
+   * @param configPath
+   *          path to PKCS#11 provider configuration file
+   * @return true if provider succesfully registered; false otherwise
+   */
+  public static boolean registerProvider(final String configPath) {
+    if (StringUtils.isEmpty(configPath)) {
+      return false;
+    }
+    final File cfgFile = new File(configPath);
+    final String absolutePath = cfgFile.getAbsolutePath();
+    if (cfgFile.isFile()) {
+      try {
+        Security.addProvider((Provider) Class.forName("sun.security.pkcs11.SunPKCS11").getConstructor(String.class)
+            .newInstance(absolutePath));
+        return true;
+      } catch (Exception e) {
+        System.err.println("Unable to register SunPKCS11 security provider.");
+        e.printStackTrace();
+      }
+    } else {
+      System.err.println("The PKCS#11 provider is not registered. Configuration file doesn't exist: " + absolutePath);
+    }
+    return false;
+  }
 }
